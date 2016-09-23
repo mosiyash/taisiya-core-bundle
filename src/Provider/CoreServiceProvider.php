0e69c72077a0b1f8a8c89fe22ee23e4dfd524aa0
Nikita Mosiyash <nikita.mosiyash@gmail.com>
2016-09-23 15:40:04 +0700

    adding App instance as service provider to Pimple

--- src/Provider/CoreServiceProvider.php
+++ src/Provider/CoreServiceProvider.php
@@ -4,11 +4,28 @@ namespace Taisiya\CoreBundle\Provider;
 
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
+use Slim\App;
 
 class CoreServiceProvider implements ServiceProviderInterface
 {
+    /**
+     * @var App
+     */
+    private $app;
+
+    /**
+     * CoreServiceProvider constructor.
+     * @param App $app
+     */
+    public function __construct(App $app)
+    {
+        $this->app = $app;
+    }
+
     public function register(Container $pimple)
     {
-        return $pimple;
+        $pimple['app'] = function (Container $pimple) {
+            return $this->app;
+        };
     }
 }
