c3bad00f9e3daf343bd616bb90a4fca7c13d939d
Nikita Mosiyash <nikita.mosiyash@gmail.com>
2016-09-23 16:31:21 +0700

    added ScriptHandler for composer

---
+++ src/Composer/ScriptHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Taisiya\CoreBundle\Composer;
+
+use Composer\Script\Event;
+
+class ScriptHandler
+{
+    public static function configureSettings(Event $event)
+    {
+        exit(':)');
+    }
+}
