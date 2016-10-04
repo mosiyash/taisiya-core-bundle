0a22f4a900b7748cae760205e1385739a77f4ada
Nikita Mosiyash <nikita.mosiyash@gmail.com>
2016-10-04 17:12:22 +0700

    up

--- app/config/settings.default.php
+++ app/config/settings.default.php
@@ -1,3 +1,41 @@
 <?php
 
-return [];
+return [
+    'phinx' => [
+        'paths' => [
+            'migrations' => dirname(__DIR__).'/db/migrations',
+            'seeds' => dirname(__DIR__).'/db/seeds',
+        ],
+        'environments' => [
+            'default_migration_table' => 'phinxlog',
+            'default_database' => 'development',
+            'production' => [
+                'adapter' => 'mysql',
+                'host' => 'localhost',
+                'name' => 'taisiya_prod',
+                'user' => 'root',
+                'pass' => 'root',
+                'port' => 3306,
+                'charset' => 'utf8',
+            ],
+            'development' => [
+                'adapter' => 'mysql',
+                'host' => 'localhost',
+                'name' => 'taisiya_dev',
+                'user' => 'root',
+                'pass' => 'root',
+                'port' => 3306,
+                'charset' => 'utf8',
+            ],
+            'testing' => [
+                'adapter' => 'mysql',
+                'host' => 'localhost',
+                'name' => 'taisiya_test',
+                'user' => 'root',
+                'pass' => 'root',
+                'port' => 3306,
+                'charset' => 'utf8',
+            ],
+        ],
+    ],
+];
